79f1851775cb26194251b8e47dbca05d6f805df0
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-10-05 11:31:16 +0100

    feat: repro dependency management issue

---
+++ src/LatestDependencies.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+// dependency that requires open zeppelin
+// import "@arbitrum/nitro-contracts/src/bridge/Inbox.sol";
+
+// dependency that does not require open zeppelin
+import "@arbitrum/nitro-contracts/src/bridge/SequencerInbox.sol";
---
+++ src/LegacyDependencies.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.6.0;
+
+// dependency that requires open zeppelin
+// import "@arbitrum/token-bridge-contracts/tokenbridge/ethereum/gateway/L1ERC20Gateway.sol";
+
+// dependency that does not require open zeppelin
+import "@arbitrum/token-bridge-contracts/tokenbridge/test/AddressMappingTest.sol";
